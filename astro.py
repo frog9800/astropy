420af92d1969d46c3ea9ab5a59b3d453015bbf50
frog9800 <mont.builder@gmail.com>
2021-06-06 00:05:08 -0500

    simple test

---
+++ astro.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Set up matplotlib
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+
+hdu_list = fits.open("SampleFITS/201102hg-16.fit")
+image_data = hdu_list[0].data
+
+plt.imshow(image_data, cmap='gray')
+plt.colorbar()
+plt.show()
