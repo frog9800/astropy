65db6a732c6083e232f829e9b2a0208ffd22e15a
frog9800 <mont.builder@gmail.com>
2021-06-27 20:12:04 -0500

    adding header

--- astro.py
+++ astro.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 from astropy.io import fits
 
-image_file = "SampleFITS/201102hg-16.fit"
+image_file = "addheadsamp/combined_dark_600.000.fit"
 hdu_list = fits.open(image_file)
 image_data = fits.getdata(image_file)
 
 plt.imshow(image_data, cmap='gray')
 plt.colorbar()
-plt.savefig('samplefit.png')
+#plt.savefig('samplefit.png')
 plt.show()
