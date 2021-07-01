bc6c155f3c936ee67d939f26ce63db1f824caefa
frog9800 <mont.builder@gmail.com>
2021-07-01 13:31:50 -0500

    combining calibrated images

--- astro.py
+++ astro.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from astropy.io import fits
 
-image_file = "addheadsamp/calibrated201103hr8047-10.fit"
+image_file = "Cal_science2/combined_science_17-19_600.000.fit"
 hdu_list = fits.open(image_file)
 image_data = fits.getdata(image_file)
 
--- comb3.py
+++ comb3.py
@@ -14,18 +14,33 @@ Sci_path2 = Path('Cal_science2')
 sci1_images = ccdp.ImageFileCollection(Sci_path1)
 sci2_images = ccdp.ImageFileCollection(Sci_path2)
 
-lights = firstreduced_images.summary['imagetyp'] == 'Light Frame'
-light_times = set(firstreduced_images.summary['exptime'][lights])
+lights = sci1_images.summary['imagetyp'] == 'Light Frame'
+light_times = set(sci1_images.summary['exptime'][lights])
+
+lights_two = sci2_images.summary['imagetyp'] == 'Light Frame'
+light_times_two = set(sci2_images.summary['exptime'][lights])
 
 
 for exp_time in sorted(light_times):
-    calibrated_science = reduced_images.files_filtered(imagetyp='Light Frame', exptime=exp_time,
+    calibrated_science = sci1_images.files_filtered(imagetyp='Light Frame', exptime=exp_time,
                                                      include_path=True)
-    combined_science = ccdp.combine(calibrated_darks,
+    combined_science = ccdp.combine(calibrated_science,
                                  method='average'
                                  )
 
     combined_science.meta['combined'] = True
 
-    science_file_name = 'combined_science_10-12{:6.3f}.fit'.format(exp_time)
-    combined_dark.write(calibrated_path / science_file_name)
+    science_file_name = 'combined_science_10-12_{:6.3f}.fit'.format(exp_time)
+    combined_science.write(Sci_path1/ science_file_name)
+
+for exp_time in sorted(light_times):
+    calibrated_science = sci2_images.files_filtered(imagetyp='Light Frame', exptime=exp_time,
+                                                     include_path=True)
+    combined_science = ccdp.combine(calibrated_science,
+                                 method='average'
+                                 )
+
+    combined_science.meta['combined'] = True
+
+    science_file_name = 'combined_science_17-19_{:6.3f}.fit'.format(exp_time)
+    combined_science.write(Sci_path2/ science_file_name)
