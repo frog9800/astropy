ca17190c3e2fc9df79e5872425d4fd08de7218b4
frog9800 <mont.builder@gmail.com>
2021-07-03 15:16:15 -0500

    practice

--- head.py
+++ head.py
@@ -1,10 +1,6 @@
 from astropy.io import fits
 
-#hdul = fits.open('addheadsamp/combined_dark_600.000.fit')
+hdul = fits.open('addheadsamp/combined_dark_600.000.fit')
 
-del hdul[1].data
-del hdul[1].header
-del hdul[2].data
-del hdul[2].header
 
 hdul.info()
