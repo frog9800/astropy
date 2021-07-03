7099dc438843091c6af8cc710a4b8069f789949d
frog9800 <mont.builder@gmail.com>
2021-07-03 15:10:00 -0500

    combining calibrated images

--- head.py
+++ head.py
@@ -1,4 +1,10 @@
 from astropy.io import fits
 
-hdul = fits.open('addheadsamp/combined_dark_600.000.fit')
-hdul[0].info
+#hdul = fits.open('addheadsamp/combined_dark_600.000.fit')
+
+del hdul[1].data
+del hdul[1].header
+del hdul[2].data
+del hdul[2].header
+
+hdul.info()
