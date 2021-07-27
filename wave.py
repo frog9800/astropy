f170abc066ecbc4b6fa360c0bc8f73173eb16d6c
frog9800 <mont.builder@gmail.com>
2021-07-26 21:06:30 -0500

    adding unified script

--- wave.py
+++ wave.py
@@ -3,17 +3,24 @@ import numpy as np
 from astropy.modeling.models import Gaussian2D
 import matplotlib.pyplot as plt
 from astropy import stats
+from astropy.io import ascii
+from astropy.table import Table
 
 with fits.open('Cal_science1/output.fit') as hdul:  # open a FITS file
     data = hdul[0].data  # assume the first extension is an image
-print(data[487:510, 0:1535])   # get the pixel value at x=5, y=2
+#print(data[488:513, 0:1535])   # get the pixel value at x=5, y=2
 
 # get values of the subsection from x=11 to 20, y=31 to 40 (inclusive)
 #print(stats.sigma_clip(data[487:510, 0:1535], sigma=2, maxiters=5))
-hdul1= data[487:510, 0:1535]
+hdp = data[488:513, 0:1535]
+pp = hdp.sum(axis=0)
 
-hdul1.writeto('Cal_science1/spec.fits', overwrite=True)
-#y, x = np.mgrid[0:1023, 0:1535]
+num = np.arange(start=1, stop=1536, step=1)
+inf = Table()
+inf['x'] = np.array(num, dtype=np.float64)
+inf['y'] = np.array(pp, dtype=np.float64)
+ascii.write(inf, 'values.txt', overwrite=True)
+
 #plt.imshow(data[487:510, 0:1535])
 
 #plt.show()
