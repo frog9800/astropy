22149dd938c2b0edc7ae6b7cabae45bc2ec3fd2d
frog9800 <mont.builder@gmail.com>
2021-08-15 07:51:56 -0500

    Complete script

--- sub.py
+++ sub.py
@@ -1,15 +1,4 @@
-from pathlib import Path
-from astropy.nddata import CCDData
-from astropy.io import fits
 from ccdproc import ImageFileCollection
-import ccdproc as ccdp
-
-data_directory = 'Cal_science1'
-data, header = fits.getdata("Cal_science1/combined_science_10-12_600.000.fit", header=True)
-
+data_directory = 'SampleFITS/'
 im_collection = ImageFileCollection(data_directory)
-
-print(im_collection.summary['object','file', 'imagetyp', 'exptime'])
-
-hdu_number = 0 # HDU means header data unit
-print(fits.getheader('Cal_science1/output.fit', hdu_number))
+print(im_collection.summary['file', 'imagetyp', 'exptime'])
